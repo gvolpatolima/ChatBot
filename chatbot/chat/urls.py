d3768615f19d484506d5621a41d9529b64c3b186
Volpato <gvolpatolima@gmail.com>
2023-04-13 23:49:01 -0300

    Adding index.html

--- chatbot/chat/urls.py
+++ chatbot/chat/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import include, path
-
-from . import views
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
